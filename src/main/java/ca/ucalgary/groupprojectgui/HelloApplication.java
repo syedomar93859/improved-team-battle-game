ba27e598ffd27f4d0f9e3ce3ba4caebe981d346c
Nethanya <nethanya.dhaiphule@ucalgary.ca>
2024-04-08 00:07:12 -0600

    Fixed error in initializing GUI

--- src/main/java/ca/ucalgary/groupprojectgui/HelloApplication.java
+++ src/main/java/ca/ucalgary/groupprojectgui/HelloApplication.java
@@ -11,8 +11,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 1200, 700);
+        stage.setTitle("Demo 3!");
         stage.setScene(scene);
         stage.show();
     }
@@ -21,3 +21,5 @@ public class HelloApplication extends Application {
         launch();
     }
 }
+
+
