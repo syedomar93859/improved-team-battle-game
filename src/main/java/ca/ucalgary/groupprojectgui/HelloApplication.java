367d1baac27ffab38446dd55587eb4bd5c1d8af2
Arfa Raja <arfa.raja@ucalgary.ca>
2024-04-11 17:18:43 -0600

    run from command line and testFiles

--- src/main/java/ca/ucalgary/groupprojectgui/HelloApplication.java
+++ src/main/java/ca/ucalgary/groupprojectgui/HelloApplication.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
@@ -18,6 +19,8 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
+        File testFile = new File(args[0]);
+        new HelloApplication();
         launch();
     }
 }
