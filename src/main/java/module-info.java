8c60ca71aae2cd31be30d4cf60723f470233ec53
Arfa Raja <arfa.raja@ucalgary.ca>
2024-04-14 22:16:17 -0600

    add tests file

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module ca.ucalgary.groupprojectgui {
     requires javafx.controls;
     requires javafx.fxml;
+    requires junit;
+    requires org.junit.jupiter.api;
 
 
     opens ca.ucalgary.groupprojectgui to javafx.fxml;
